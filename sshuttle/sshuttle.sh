113290f8c6720ff100da992720026b91eab1b1dc
Masoud Paydarsani <m.paydarsani@outlook.com>
2023-09-01 13:17:57 +0330

    adding sshuttle scripts

---
+++ sshuttle/sshuttle.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+sudo apt install -y sshuttle 
+
+touch v{p,d,s}
+chmod +x v{p,d,s}
+
+cat << EOF > vp
+#!/bin/bash
+sshuttle --dns -r user@ip:port 0/0 -x ip --no-latency-control  &
+EOF
+
+cat << 'EOF' > vd
+#!/bin/bash
+sudo kill -9 $(pgrep sshuttle)
+EOF
+
+cat << 'EOF' > vs
+#!/bin/bash
+sudo pgrep sshuttle
+
+if [ $? -ne 0 ]; then
+	echo -e "VPN is disconnected"
+else
+	echo -e "VPN is running"
+fi
+EOF
+
+echo 'export PATH="$PATH:$HOME"' >> ~/.bashrc
+source .bashrc
